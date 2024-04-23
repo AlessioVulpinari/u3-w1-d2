1b0a6c1ab86e7aea53ebbd6de2ad65ebed8b8327
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-04-23 16:33:39 +0200

    added state to change genre

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,15 +1,41 @@
 import { Component } from "react"
 import Card from "react-bootstrap/Card"
 import Badge from "react-bootstrap/Badge"
-import books from "../data/books/fantasy.json"
+import Button from "react-bootstrap/Button"
+import ButtonGroup from "react-bootstrap/ButtonGroup"
+import fantasy from "../data/books/fantasy.json"
+import history from "../data/books/history.json"
+import horror from "../data/books/horror.json"
+import romance from "../data/books/romance.json"
+import scifi from "../data/books/scifi.json"
 import { Col, Container, Row } from "react-bootstrap"
 
 class AllTheBooks extends Component {
+  state = {
+    genre: fantasy,
+  }
   render() {
     return (
       <Container>
+        <ButtonGroup aria-label='Basic example' className='d-flex gap-1 my-2'>
+          <Button variant='secondary' onClick={() => this.setState({ genre: fantasy })}>
+            Fantasy{" "}
+          </Button>
+          <Button variant='secondary' onClick={() => this.setState({ genre: history })}>
+            History
+          </Button>
+          <Button variant='secondary' onClick={() => this.setState({ genre: horror })}>
+            Horror
+          </Button>
+          <Button variant='secondary' onClick={() => this.setState({ genre: romance })}>
+            Romance
+          </Button>
+          <Button variant='secondary' onClick={() => this.setState({ genre: scifi })}>
+            Sci-fi
+          </Button>
+        </ButtonGroup>
         <Row className='row-cols-xl-4 row-cols-md-3 row-cols-sm-2 row-cols-1'>
-          {books.map((book) => {
+          {this.state.genre.map((book) => {
             return (
               <Col key={book.asin}>
                 <Card>
